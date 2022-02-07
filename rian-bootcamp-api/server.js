1b16c627c546b0d4dcb7ca2538e26dc7aa926ed7
Rianto <rianto.rnt@gmail.com>
2022-02-07 12:50:47 +0800

    Creating Routes and Response in Express #1

--- rian-bootcamp-api/server.js
+++ rian-bootcamp-api/server.js
@@ -6,6 +6,10 @@ dotenv.config({ path: './config/config.env' });
 
 const app = express();
 
+app.get('/', (req, res) => {
+  res.status(200).json({ success: true, data: { id: 1 } });
+});
+
 const PORT = process.env.PORT || 5000;
 
 app.listen(

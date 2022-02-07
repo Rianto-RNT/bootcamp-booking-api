287bf3afdbd4caafe3b2e166e68d7913e7d4df63
Rianto <rianto.rnt@gmail.com>
2022-02-07 14:04:27 +0800

    using Express Router

--- rian-bootcamp-api/server.js
+++ rian-bootcamp-api/server.js
@@ -1,30 +1,16 @@
 const express = require('express');
 const dotenv = require('dotenv');
 
+// Route files
+const bootcamps = require('./routes/bootcamps');
+
 // Load env vars
 dotenv.config({ path: './config/config.env' });
 
 const app = express();
 
-app.get('/api/v1/bootcamps', (req, res) => {
-  res.status(200).json({ success: true, message: 'Show all bootcamps' });
-});
-
-app.get('/api/v1/bootcamps/:id', (req, res) => {
-    res.status(200).json({ success: true, message: `Show single bootcamp ${req.params.id}` });
-  });  
-
-app.post('/api/v1/bootcamps', (req, res) => {
-  res.status(200).json({ success: true, message: 'Create new bootcamp' });
-});
-
-app.put('/api/v1/bootcamps/:id', (req, res) => {
-  res.status(200).json({ success: true, message: `Update bootcamp ${req.params.id}` });
-});
-
-app.delete('/api/v1/bootcamps/:id', (req, res) => {
-  res.status(200).json({ success: true, message: `Delete bootcamp ${req.params.id}` });
-});
+// Mount routers
+app.use('/api/v1/bootcamps', bootcamps);
 
 const PORT = process.env.PORT || 5000;
 

ada8c107ed6fe2f7ddb7aad7873e38231bf6a256
Rianto <rianto.rnt@gmail.com>
2022-02-07 13:23:17 +0800

    Creating Routes and Response in Express #2

--- rian-bootcamp-api/server.js
+++ rian-bootcamp-api/server.js
@@ -6,8 +6,24 @@ dotenv.config({ path: './config/config.env' });
 
 const app = express();
 
-app.get('/', (req, res) => {
-  res.status(200).json({ success: true, data: { id: 1 } });
+app.get('/api/v1/bootcamps', (req, res) => {
+  res.status(200).json({ success: true, message: 'Show all bootcamps' });
+});
+
+app.get('/api/v1/bootcamps/:id', (req, res) => {
+    res.status(200).json({ success: true, message: `Show single bootcamp ${req.params.id}` });
+  });  
+
+app.post('/api/v1/bootcamps', (req, res) => {
+  res.status(200).json({ success: true, message: 'Create new bootcamp' });
+});
+
+app.put('/api/v1/bootcamps/:id', (req, res) => {
+  res.status(200).json({ success: true, message: `Update bootcamp ${req.params.id}` });
+});
+
+app.delete('/api/v1/bootcamps/:id', (req, res) => {
+  res.status(200).json({ success: true, message: `Delete bootcamp ${req.params.id}` });
 });
 
 const PORT = process.env.PORT || 5000;

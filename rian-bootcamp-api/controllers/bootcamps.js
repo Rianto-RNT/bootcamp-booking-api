c3698fda2a2e988c025163b7844773ef64929704
Rianto <rianto.rnt@gmail.com>
2022-02-08 20:23:11 +0800

    Error Handler middleware

--- rian-bootcamp-api/controllers/bootcamps.js
+++ rian-bootcamp-api/controllers/bootcamps.js
@@ -37,9 +37,10 @@ exports.getSingleBootcamp = async (req, res, next) => {
       data: bootcamp
     });
   } catch (err) {
-    res.status(400).json({
-      success: false
-    });
+    // res.status(400).json({
+    //   success: false
+    // });
+    next(err);
   }
 };
 

880841bfc5192558413317eb283024f1e71b9367
Rianto <rianto.rnt@gmail.com>
2022-02-08 14:05:07 +0800

    Create Bootcamp GET method and getting from database

--- rian-bootcamp-api/controllers/bootcamps.js
+++ rian-bootcamp-api/controllers/bootcamps.js
@@ -3,17 +3,43 @@ const Bootcamp = require('../models/Bootcamp');
 // @desc    Show all bootcamps
 // @route   GET /api/v1/bootcamps
 // @access  Public
-exports.getBootcamps = (req, res, next) => {
-  res.status(200).json({ success: true, message: 'Show all bootcamps' });
+exports.getBootcamps = async (req, res, next) => {
+  try {
+    const bootcamps = await Bootcamp.find(req.params);
+
+    res.status(200).json({
+      success: true,
+      data: bootcamps
+    });
+  } catch (err) {
+    res.status(400).json({
+      success: false
+    });
+  }
 };
 
 // @desc    Show single bootcamp
 // @route   GET /api/v1/bootcamps/:id
 // @access  Public
-exports.getSingleBootcamp = (req, res, next) => {
-  res
-    .status(200)
-    .json({ success: true, message: `Show single bootcamp ${req.params.id}` });
+exports.getSingleBootcamp = async (req, res, next) => {
+  try {
+    const bootcamp = await Bootcamp.findById(req.params.id);
+
+    if (!bootcamp) {
+      return res.status(400).json({
+        success: false
+      });
+    }
+
+    res.status(200).json({
+      success: true,
+      data: bootcamp
+    });
+  } catch (err) {
+    res.status(400).json({
+      success: false
+    });
+  }
 };
 
 // @desc    Create bootcamp
@@ -37,10 +63,19 @@ exports.createBootcamp = async (req, res, next) => {
 // @desc    Update bootcamp
 // @route   PUT /api/v1/bootcamps/:id
 // @access  Private
-exports.updateBootcamp = (req, res, next) => {
-  res
-    .status(200)
-    .json({ success: true, message: `Update bootcamp ${req.params.id}` });
+exports.updateBootcamp = async (req, res, next) => {
+  try {
+    const bootcamp = await Bootcamp.findByIdAndUpdate(req.body.id);
+
+    res.status(200).json({
+      success: true,
+      data: bootcamp
+    });
+  } catch (err) {
+    res.status(400).json({
+      success: false
+    });
+  }
 };
 
 // @desc    Remove bootcamp

362849736e1179e90ca9dd94f3735502f2dfc62b
Rianto <rianto.rnt@gmail.com>
2022-02-08 23:40:29 +0800

    implementing Async Await Middleware

--- rian-bootcamp-api/middleware/error.js
+++ rian-bootcamp-api/middleware/error.js
@@ -10,20 +10,21 @@ const errorHandler = (err, req, res, next) => {
 
   // Mongoose bad ObjectId
   if (err.name === 'CastError') {
-    const message = `Resource not found with _id of ${err.value}. Please add correct _id`;
+    const message = `Resource not found with id of ${err.value}. Please add correct id`;
     error = new ErrorResponse(message, 404);
   }
 
   // Mongoose duplicate key
-  if (err.code = 1100) {
-      const message = 'Duplicate field value entered! Please add different name for the Bootcamp.';
-      error = new ErrorResponse(message, 400);
+  if (err.code = 11000) {
+    const message =
+      'Duplicate field value entered! Please add different name for the Bootcamp.';
+    error = new ErrorResponse(message, 400);
   }
 
   // Mongoose validation error
   if (err.name === 'ValidationError') {
-      const message = Object.values(err.errors).map(val => val.message);
-      error = new ErrorResponse(message, 400)
+    const message = Object.values(err.errors).map(val => val.message);
+    error = new ErrorResponse(message, 400);
   }
 
   res.status(error.statusCode || 500).json({

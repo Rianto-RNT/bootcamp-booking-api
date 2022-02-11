c101db8a4812b29e4b33938d921cc4a3197cc1d4
Rianto <rianto.rnt@gmail.com>
2022-02-11 11:48:47 +0800

    Get single course and Add Course to specific bootcamp

--- rian-bootcamp-api/routes/courses.js
+++ rian-bootcamp-api/routes/courses.js
@@ -1,8 +1,17 @@
 const express = require('express');
-const { getCourses } = require('../controllers/courses');
+const {
+  getCourses,
+  getSingleCourse,
+  addCourse
+} = require('../controllers/courses');
 
 const router = express.Router({ mergeParams: true });
 
-router.route('/').get(getCourses);
+router
+  .route('/')
+  .get(getCourses)
+  .post(addCourse);
+  
+router.route('/:id').get(getSingleCourse);
 
 module.exports = router;

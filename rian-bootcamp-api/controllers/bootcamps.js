a690cde9345b834c2ffecee2b760ff51b77b7f69
Rianto <rianto.rnt@gmail.com>
2022-02-11 11:19:08 +0800

    Populate, Virtuals and Cascade Delete for Bootcamp and the courses

--- rian-bootcamp-api/controllers/bootcamps.js
+++ rian-bootcamp-api/controllers/bootcamps.js
@@ -25,7 +25,7 @@ exports.getBootcamps = asyncHandler(async (req, res, next) => {
   queryStr = queryStr.replace(/\b(gt|gte|lt|lte|in)\b/g, match => `$${match}`);
 
   // Finding resource
-  query = Bootcamp.find(JSON.parse(queryStr));
+  query = Bootcamp.find(JSON.parse(queryStr)).populate('courses');
 
   // Select Fields
   if (req.query.select) {
@@ -136,7 +136,7 @@ exports.updateBootcamp = asyncHandler(async (req, res, next) => {
 // @route   Delete /api/v1/bootcamps/:id
 // @access  Private
 exports.deleteBootcamp = asyncHandler(async (req, res, next) => {
-  const bootcamp = await Bootcamp.findByIdAndDelete(req.params.id);
+  const bootcamp = await Bootcamp.findById(req.params.id);
 
   if (!bootcamp) {
     return next(
@@ -146,6 +146,8 @@ exports.deleteBootcamp = asyncHandler(async (req, res, next) => {
       )
     );
   }
+
+  bootcamp.remove();
 
   res.status(200).json({
     success: true,

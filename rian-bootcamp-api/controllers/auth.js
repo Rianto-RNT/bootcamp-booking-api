8ad83d57aa9841339e0a4e21a4cddf95706ffc75
Rianto <rianto.rnt@gmail.com>
2022-02-14 19:14:10 +0800

    Sending JWT in a Cookie

--- rian-bootcamp-api/controllers/auth.js
+++ rian-bootcamp-api/controllers/auth.js
@@ -1,6 +1,7 @@
 const ErrorResponse = require('../utils/errorResponse');
 const asyncHandler = require('../middleware/async');
 const User = require('../models/User');
+const cookieParser = require('cookie-parser');
 
 // @desc    Register User
 // @route   POST /api/v1/auth/register
@@ -16,10 +17,7 @@ exports.register = asyncHandler(async (req, res, next) => {
     role
   });
 
-  // Create Token
-  const token = user.getSignedJwtToken();
-
-  res.status(200).json({ success: true, token });
+  sendTokenResponse(user, 200, res);
 });
 
 // @desc    Login User
@@ -47,8 +45,30 @@ exports.login = asyncHandler(async (req, res, next) => {
     return next(new ErrorResponse('Invalid Credentials', 401));
   }
 
+  sendTokenResponse(user, 200, res);
+});
+
+// Get token from model, create cookie and send response
+const sendTokenResponse = (user, statusCode, res) => {
   // Create Token
   const token = user.getSignedJwtToken();
 
-  res.status(200).json({ success: true, token });
-});
+  const options = {
+    expires: new Date(
+      Date.now() + process.env.JWT_COOKIE_EXPIRE * 24 * 60 * 60 * 1000
+    ),
+    httpOnly: true
+  };
+
+  if (process.env.NODE_ENV === 'production') {
+    options.secure = true;
+  }
+
+  res
+    .status(statusCode)
+    .cookie('token', token, options)
+    .json({
+      success: true,
+      token
+    });
+};

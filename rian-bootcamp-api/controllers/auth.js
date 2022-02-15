966f36b7e211ffeb60e184ac9fcf8b6e5ed221f4
Rianto <rianto.rnt@gmail.com>
2022-02-15 21:12:33 +0800

    Update User Details

--- rian-bootcamp-api/controllers/auth.js
+++ rian-bootcamp-api/controllers/auth.js
@@ -58,6 +58,40 @@ exports.getMe = asyncHandler(async (req, res, next) => {
   res.status(200).json({ sucess: true, data: user });
 });
 
+// @desc    Update User details
+// @route   PUT /api/v1/auth/updatedetails
+// @access  Private
+exports.updateDetails = asyncHandler(async (req, res, next) => {
+  const fieldsToUpdate = {
+    name: req.body.name,
+    email: req.body.email
+  };
+
+  const user = await User.findByIdAndUpdate(req.user.id, fieldsToUpdate, {
+    new: true,
+    runValidators: true
+  });
+
+  res.status(200).json({ sucess: true, data: user });
+});
+
+// @desc    Update user password
+// @route   PUT /api/v1/auth/updatepassword
+// @access  Private
+exports.updatePassword = asyncHandler(async (req, res, next) => {
+  const user = await User.findById(req.user.id).select('+password');
+
+  // check current password
+  if (!(await user.matchPassword(req.body.currentPassword))) {
+    return next(new ErrorResponse('Password is incorrect', 401));
+  }
+
+  user.password = req.body.newPassword;
+  await user.save();
+
+  sendTokenResponse(user, 200, res);
+});
+
 // @desc    Forgot password
 // @route   POST /api/v1/auth/forgotpassword
 // @access  Public

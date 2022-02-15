89d393421576748054045122374c2d7c1fc9e98a
Rianto <rianto.rnt@gmail.com>
2022-02-15 20:02:25 +0800

    Forgot password - send email (nodemailer with mailtrap)

--- rian-bootcamp-api/controllers/auth.js
+++ rian-bootcamp-api/controllers/auth.js
@@ -1,7 +1,7 @@
 const ErrorResponse = require('../utils/errorResponse');
 const asyncHandler = require('../middleware/async');
+const sendEmail = require('../utils/sendEmail');
 const User = require('../models/User');
-const cookieParser = require('cookie-parser');
 
 // @desc    Register User
 // @route   POST /api/v1/auth/register
@@ -71,6 +71,31 @@ exports.forgotPassword = asyncHandler(async (req, res, next) => {
   const resetToken = user.getResetPasswordToken();
 
   await user.save({ validateBeforeSave: false });
+
+  // Create reset url
+  const resetUrl = `${req.protocol}://${req.get(
+    'host'
+  )}/api/v1/resetpassword/${resetToken}`;
+
+  const message = `You are receiving this email because you (for someone else) has requested the reset of a password. Please make a PUT request to \n\n ${resetUrl}`;
+
+  try {
+    await sendEmail({
+      email: user.email,
+      subject: 'Password reset token',
+      message
+    });
+
+    res.status(200).json({ success: true, data: 'Email send' });
+  } catch (err) {
+    console.log(err);
+    user.resetPasswordToken = undefined;
+    user.resetPasswordExpire = undefined;
+
+    await user.save({ validateBeforeSave: false });
+
+    return next(new ErrorResponse('Email could not be sent', 500));
+  }
 
   res.status(200).json({ sucess: true, data: user });
 });

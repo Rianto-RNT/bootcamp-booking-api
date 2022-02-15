4d4318ccd905ae3800ef106637c0ad9534913985
Rianto <rianto.rnt@gmail.com>
2022-02-15 22:01:51 +0800

    User CRUD (admin only)

---
+++ rian-bootcamp-api/routes/users.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const {
+  getUsers,
+  getSingleUser,
+  createUser,
+  updateUser,
+  deleteUser
+} = require('../controllers/users');
+
+const User = require('../models/User');
+
+const router = express.Router({ mergeParams: true });
+
+const advancedResults = require('../middleware/advancedResults');
+const { protect, authorize } = require('../middleware/auth');
+
+router.use(protect);
+router.use(authorize('admin'));
+
+router
+  .route('/')
+  .get(advancedResults(User), getUsers)
+  .post(createUser);
+
+router
+  .route('/:id')
+  .get(getSingleUser)
+  .put(updateUser)
+  .delete(deleteUser);
+
+module.exports = router;

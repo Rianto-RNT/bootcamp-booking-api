4c8fecf7808c6a793376cea970f2d747ed95ad2d
Rianto <rianto.rnt@gmail.com>
2022-02-16 09:02:08 +0800

    Review Model and Get Reviews (controller and route)

--- rian-bootcamp-api/routes/bootcamps.js
+++ rian-bootcamp-api/routes/bootcamps.js
@@ -14,6 +14,7 @@ const Bootcamp = require('../models/Bootcamp');
 
 // Include other resource routers
 const courseRouter = require('./courses');
+const reviewRouter = require('./reviews');
 
 const router = express.Router();
 
@@ -22,6 +23,7 @@ const { protect, authorize } = require('../middleware/auth');
 
 // Re-routes into other resourse routers
 router.use('/:bootcampId/courses', courseRouter);
+router.use('/:bootcampId/reviews', reviewRouter);
 
 router.route('/radius/:zipode/:distance').get(getBootcampsInRadius);
 

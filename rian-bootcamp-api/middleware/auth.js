83cd3fc551ed830b7973d754e7fa76e42c96bde1
Rianto <rianto.rnt@gmail.com>
2022-02-17 14:00:01 +0800

    Logot to clear token cookie

--- rian-bootcamp-api/middleware/auth.js
+++ rian-bootcamp-api/middleware/auth.js
@@ -11,12 +11,12 @@ exports.protect = asyncHandler(async (req, res, next) => {
     req.headers.authorization &&
     req.headers.authorization.startsWith('Bearer')
   ) {
+    // Set token from Bearer token in header
     token = req.headers.authorization.split(' ')[1];
+    // Set token from cookie
+  } else if (req.cookies.token) {
+    token = req.cookies.token;
   }
-
-  //   else if (req.cookies.token) {
-  //       token = req.cookies.token
-  //   }
 
   // Make sure token exists
   if (!token) {
@@ -33,7 +33,7 @@ exports.protect = asyncHandler(async (req, res, next) => {
 
     next();
   } catch (err) {
-    return next(new ErrorResponse('Not authorize to access this route', 401));
+    return next(new ErrorResponse('Not authorized to access this route', 401));
   }
 });
 
@@ -43,7 +43,7 @@ exports.authorize = (...roles) => {
     if (!roles.includes(req.user.role)) {
       return next(
         new ErrorResponse(
-          `User role * ${req.user.role} * is not authorize to access this route`,
+          `User role * ${req.user.role} * is not authorized to access this route`,
           403
         )
       );
